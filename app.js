072120ee0739d02890802b928475dc20f0f936d2
Sergio Flores <sergio.israel.hernandez@gmail.com>
2024-03-18 13:26:11 -0600

    change | app permite componentes async

--- app.js
+++ app.js
@@ -3,43 +3,42 @@ import { AcercaComponent } from "./components/AcercaComponent.js";
 import { ContactoComponent } from "./components/ContactoComponent.js";
 
 class Router {
-    constructor(rutas) {
-      this.rutas = rutas;
-      this.contenido = document.getElementById("contenido");
-      this.iniciar();
-    }
-  
-    iniciar() {
-      window.addEventListener("hashchange", () => {
-        const hashActual = window.location.hash.substring(1);
-        const rutaActual = this.rutas.find((ruta) => ruta.path === hashActual);
-        if (!rutaActual) return;
-        this.renderizar(rutaActual.componente);
-      });
-  
-      const rutaInicio = this.rutas.find((ruta) => ruta.path === "/");
-      this.renderizar(rutaInicio.componente);
-    }
-  
-    renderizar(componente) {
-      this.contenido.innerHTML = componente();
-    }
+  constructor(rutas) {
+    this.rutas = rutas;
+    this.contenido = document.getElementById("contenido");
+    this.iniciar();
   }
-  
-  const rutas = [
-    {
-      path: "/",
-      componente: InicioComponent,
-    },
-    {
-      path: "/acerca",
-      componente: AcercaComponent,
-    },
-    {
-      path: "/contacto",
-      componente: ContactoComponent,
-    },
-  ];
 
-  const router = new Router(rutas);
-  
+  async iniciar() {
+    window.addEventListener("hashchange", async () => {
+      const hashActual = window.location.hash.substring(1);
+      const rutaActual = this.rutas.find((ruta) => ruta.path === hashActual);
+      if (!rutaActual) return;
+      await this.renderizar(rutaActual.componente);
+    });
+
+    const rutaInicio = this.rutas.find((ruta) => ruta.path === "/");
+    await this.renderizar(rutaInicio.componente);
+  }
+
+  async renderizar(componente) {
+    this.contenido.innerHTML = await componente();
+  }
+}
+
+const rutas = [
+  {
+    path: "/",
+    componente: InicioComponent,
+  },
+  {
+    path: "/acerca",
+    componente: AcercaComponent,
+  },
+  {
+    path: "/contacto",
+    componente: ContactoComponent,
+  },
+];
+
+const router = new Router(rutas);
